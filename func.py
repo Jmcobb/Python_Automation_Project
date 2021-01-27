68991b28209ef1204c82b2c84afc977f1e9b8da0
James Cobb <J.morgan.cobb@gmail.com>
2021-01-27 14:25:07 -0600

    code and readme changes

--- func.py
+++ func.py
@@ -1,8 +1,13 @@
-# Here is to show some basic logic w/ python! 
+# Here is to show my logic skills w/ python! 
 
 # Also, here is an example of good practice which results in 
-# cleaner code by having a module containing this python function.
-# I believe it is called "Seperation of Concerns"
+# cleaner code by having a module containing this python functions.
+
+import auto_build
+import boto3
+
+def nl(): 
+    print('\n')
 
 def yes_or_no():
     print('Are you running the program? y/n')
@@ -11,9 +16,15 @@ def yes_or_no():
         print('Good, time to get to the fun part!')
     elif x[-1] == 'n':
         print('Lol, I am glad to see you have a sense of humor.')
+        nl()
         yes_or_no()
     else:
         print('That does not seem like an option to me... Try again!')
-        yes_or_no()
+        nl()
+        yes_or_no() 
 
 # I hope whoever grades this laugh as much as I did. 
+
+def create_vm(): 
+    print('Time to create a VM based on the YAML provided.')
+    
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import yes_no
+# Welcome to the main script. 
+
+import func
 
 print('Hello, User!')
 print('Welcome to my code project.')
-print('This program will meet the following criteria:')
+func.nl()
+
+print('This program was built to meet the following criteria: ')
+func.nl()
 
 print('1. You will be able to run the program!')
-yes_no.yes_or_no()
+func.yes_or_no()
+func.nl()
 
-print('2. You will be able to deploy a VM, based on a given YAML file.')
+print('2. Next, you will be able to deploy a VM, based on a given YAML file.')
+func.create_vm()
+func.nl()
+
+print('3. You will be able to SSH into the new instance as new_user1 and new_user2.')
+func.nl()
+
+print('4. Read and write from the two volumes attached to the VM instance.')
+func.nl()
